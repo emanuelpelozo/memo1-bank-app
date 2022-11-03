068fdd8cf5f8feba1413368ae388bc26119fd325
emanuelpelozo <emanuel.pelozo@outlook.com>
2022-11-03 20:36:45 -0300

    added promotion calculation

---
+++ src/main/java/com/aninfo/service/PromotionService.java
@@ -0,0 +1,24 @@
+package com.aninfo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PromotionService {
+
+    private static final Double PROMOTION_CANDIDATE_LIMIT = 2000.0;
+    private static final Double PROMOTION_REWARD_LIMIT = 500.0;
+    private static final Double PROMOTION_REWARD_PERCENTAGE = 0.10;
+
+    public Double applyPromotion(final Double depositAmount) {
+        return depositAmount < PROMOTION_CANDIDATE_LIMIT
+                ? depositAmount
+                : depositAmount + getReward(depositAmount);
+    }
+
+    private Double getReward(final Double depositAmount) {
+        final double reward = depositAmount * PROMOTION_REWARD_PERCENTAGE;
+        return reward > PROMOTION_REWARD_LIMIT
+                ? PROMOTION_REWARD_LIMIT
+                : reward;
+    }
+}
